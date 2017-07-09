0cdc5eb6342f70d3e424e4543cb3dae70fef5c61
Andrei Dyminski <smoke59@mail.ru>
2017-07-09 22:19:15 +0300

    full code

---
+++ build/js/script.js
@@ -0,0 +1,9 @@
+$(window).on("load", function () {
+  $(".date").datepicker({
+    showOn: "button",
+    buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
+    buttonImageOnly: true,
+    buttonText: "Select date"
+  });
+});
+
